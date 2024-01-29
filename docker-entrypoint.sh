5884689e0b6f8a21f849e23aa859ceaef50cb2db
Reboot <sergejskorikjr@gmail.com>
2024-01-29 14:53:16 +0200

    docs: update docs for docker-entrypoint file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,15 +3,19 @@ set -e
 
 export DJANGO_SETTINGS_MODULE=ecommerce_backend.settings.local
 
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Apply migrations
+# Apply django migrations
 until python3 manage.py migrate
 do
-  echo "Waiting for database to be ready"
+  echo "Waiting for the database to be ready..."
+  sleep 2
 done
+
+echo "Database is ready. Starting the development server..."
 
 # Start the development server
 gunicorn ecommerce_backend.wsgi:application --bind 0.0.0.0:8000 --workers 4 --threads 4
 
-exec $@
+exec "$@"
