a9366bcc359e2bf49104e282def2870d27408b95
Reboot <sergejskorikjr@gmail.com>
2024-01-31 17:51:18 +0200

    feat: update category-related views and serialzers
    - add CategoryListSerializer and SubcategorySerializer
    - rename CategorySerializer to CategoryDetailSerializer
    - refactor category views to reflect serializer changes
    - remove get() from the CategoryDetailView, since it is no longer needed

--- apps/product/serializers/category.py
+++ apps/product/serializers/category.py
@@ -6,14 +6,41 @@ from rest_framework import serializers
 from apps.product.models import Category
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class SubcategorySerializer(serializers.ModelSerializer):
     """
-    Serializer for Category model.
+    Serializer for representing a subcategory in a simplified manner.
     """
 
+    class Meta:
+        model = Category
+        fields = ["id", "name", "slug", "level"]
+        read_only_fields = ["id", "parent"]
+
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    """
+    Serializer for detailed representation of a Category, including subcategories.
+    """
+
+    parent = SubcategorySerializer()
+    subcategories = SubcategorySerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = ["id", "name", "slug", "parent", "level", "subcategories"]
+        depth = 1
+        read_only_fields = ["id", "parent"]
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    """
+    Serializer for simplified representation of a Category without subcategories.
+    """
+
+    parentId = serializers.UUIDField(source="parent.id", read_only=True, allow_null=True)
     parentSlug = serializers.SlugField(source="parent.slug", read_only=True, allow_null=True)
 
     class Meta:
         model = Category
-        fields = ["id", "slug", "name", "level", "parentSlug"]
+        fields = ["id", "name", "slug", "level", "parentId", "parentSlug"]
         read_only_fields = ["id"]
--- apps/product/views/category.py
+++ apps/product/views/category.py
@@ -2,12 +2,12 @@
 This module contains Category-related views.
 """
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from apps.product.models import Category
-from apps.product.serializers.category import CategorySerializer
+from apps.product.serializers.category import CategoryDetailSerializer, CategoryListSerializer
 
 
 class CategoryDetailView(generics.RetrieveAPIView):
@@ -15,7 +15,7 @@ class CategoryDetailView(generics.RetrieveAPIView):
     API View for retrieving single category with its descendants.
     """
 
-    serializer_class = CategorySerializer
+    serializer_class = CategoryDetailSerializer
 
     def get_object(self):
         """
@@ -33,48 +33,12 @@ class CategoryDetailView(generics.RetrieveAPIView):
                 descendant_category = get_object_or_404(
                     Category, parent=parent_category, slug=subcategory_slug, level=1
                 )
-                return descendant_category.subcategories.all()
+                return descendant_category
             else:
-                return parent_category.subcategories.all()
+                return parent_category
         else:
             # If no category slug is provided, return top-level categories
-            return Category.objects.filter(level=0)
-
-    def get(self, request, *args, **kwargs):
-        """
-        Get view for retrieving a category and its descendants.
-
-        Retrieves the object, serializes it, and includes information about the requested
-        category and its descendants in the response.
-
-        :param request: The HTTP request.
-        :param args: Variable-length argument list.
-        :param kwargs: Arbitrary keyword arguments.
-        :return: A Response object containing serialized data with information about the requested
-        category and its descendants.
-        If the category or its descendants are not found, returns an appropriate HTTP 404 response.
-        """
-        children_categories = self.get_object()
-        serializer = self.get_serializer(children_categories, many=True)
-
-        category_slug = self.kwargs.get("category_slug")
-        subcategory_slug = self.kwargs.get("subcategory_slug")
-
-        requested_category = None
-
-        if subcategory_slug:
-            requested_category = get_object_or_404(Category, slug=subcategory_slug)
-        elif category_slug:
-            requested_category = get_object_or_404(Category, slug=category_slug)
-
-        response_data = {
-            "category": CategorySerializer(requested_category).data
-            if requested_category
-            else None,
-            "descendants": serializer.data,
-        }
-
-        return Response(response_data)
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 class CategoryListView(generics.ListAPIView):
@@ -82,7 +46,7 @@ class CategoryListView(generics.ListAPIView):
     API View for retrieving list of categories.
     """
 
-    serializer_class = CategorySerializer
+    serializer_class = CategoryListSerializer
     queryset = Category.objects.prefetch_related("subcategories")
     pagination_class = PageNumberPagination
     page_size = 100
