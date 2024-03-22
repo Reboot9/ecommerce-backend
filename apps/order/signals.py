795bd2615f7cd3838602b82b135e8de64c210585
Reboot <sergejskorikjr@gmail.com>
2024-03-22 20:54:29 +0200

    feat: add signals for warehouse managing related to orders

--- apps/order/signals.py
+++ apps/order/signals.py
@@ -1,14 +1,14 @@
 """
-Module: order_signals.
-
-This module contains Django signals related to the 'Order' model in the 'order' app.
+Django signals related to the order app.
 """
 
 from django.db.models import Max
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
 from apps.order.models.order import Order
+from apps.order.models.order_item import OrderItem
+from apps.order.utils import create_reserve, create_or_update_transaction
 
 
 @receiver(pre_save, sender=Order)
@@ -19,3 +19,81 @@ def update_order_number(sender, instance, **kwargs):
             "order_number_max"
         ]
         instance.order_number = max_value + 1 if max_value is not None else 1
+
+
+@receiver(post_save, sender=Order)
+def manage_order_items_transactions(sender, instance, created, **kwargs):
+    """
+    Signal to create reserves or update transactions based on the order status.
+    """
+    from apps.warehouse.models import Transaction
+
+    order_status = instance.status
+
+    # Define the mapping of order statuses to transaction types
+    status_transaction_mapping = {
+        Order.OrderStatusChoices.NEW: Transaction.TransactionTypeChoices.ORDER,
+        Order.OrderStatusChoices.PROCESSING: Transaction.TransactionTypeChoices.ORDER,
+        Order.OrderStatusChoices.SENT: Transaction.TransactionTypeChoices.ORDER,
+        Order.OrderStatusChoices.DELIVERED: Transaction.TransactionTypeChoices.ORDER,
+        Order.OrderStatusChoices.EXECUTED: Transaction.TransactionTypeChoices.ORDER,
+        Order.OrderStatusChoices.CANCELED: Transaction.TransactionTypeChoices.RETURN,
+        Order.OrderStatusChoices.RETURNED: Transaction.TransactionTypeChoices.RETURN,
+        Order.OrderStatusChoices.ISSUE: Transaction.TransactionTypeChoices.RETURN,
+    }
+
+    # Get the transaction type based on the order status
+    transaction_type = status_transaction_mapping.get(order_status)
+
+    # Create or update transaction based on the order status
+    if transaction_type:
+        for item in instance.items.all():
+            if order_status in [Order.OrderStatusChoices.NEW, Order.OrderStatusChoices.PROCESSING]:
+                create_reserve(instance, item)
+            create_or_update_transaction(instance, item, transaction_type=transaction_type)
+
+
+@receiver(post_save, sender=OrderItem)
+def create_reserves_for_order_item(sender, instance, created, **kwargs):
+    """
+    Utility signal to create model instances when creating an OrderItem.
+    """
+    from apps.warehouse.models import Reserve, Transaction
+
+    if created:
+        order = instance.order
+        order_status = order.status
+        if order_status in [
+            Order.OrderStatusChoices.NEW,
+            Order.OrderStatusChoices.PROCESSING,
+        ]:
+            Reserve.objects.create(
+                order=order, reserved_item=instance.product, quantity=instance.quantity
+            )
+        else:
+            # Determine transaction type and is_active flag based on order status
+            is_active = order_status not in [
+                Order.OrderStatusChoices.EXECUTED,
+                Order.OrderStatusChoices.ISSUE,
+            ]
+            transaction_type = (
+                Transaction.TransactionTypeChoices.ORDER
+                if order_status
+                not in (Order.OrderStatusChoices.ISSUE, Order.OrderStatusChoices.EXECUTED)
+                else Transaction.TransactionTypeChoices.RETURN
+            )
+
+            # Create transaction for the order item
+            Transaction.objects.create(
+                product=instance.product,
+                order_item=instance,
+                quantity=instance.quantity,
+                transaction_type=transaction_type,
+                is_active=is_active,
+            )
+
+            # Deactivate related reserve
+            Reserve.objects.get(
+                order=order,
+                reserved_item=instance.product,
+            ).update(is_active=False)
--- apps/order/utils.py
+++ apps/order/utils.py
@@ -1,7 +1,5 @@
 """
-Module: phone_validators.
-
-This module contains a Django validator for phone numbers.
+Utility functions for Order app.
 """
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
@@ -12,3 +10,54 @@ phone_validator = RegexValidator(
         "Phone number must be entered in the format: '+38(050)111-11-11'. Up to 17 digits allowed."
     ),
 )
+
+
+def create_reserve(order_instance, item):
+    """
+    Create a reserve for the given order item.
+    """
+    from apps.warehouse.models import Reserve
+
+    reserve, _ = Reserve.objects.get_or_create(
+        order=order_instance, reserved_item=item.product, quantity=item.quantity
+    )
+    reserve.save()
+
+
+def create_or_update_transaction(order_instance, item, *, transaction_type):
+    """
+    Create or update a transaction for the given order item.
+    """
+    from apps.warehouse.models import Reserve, Transaction
+    from apps.order.models.order import Order
+
+    is_active = order_instance.status not in [
+        Order.OrderStatusChoices.NEW,
+        Order.OrderStatusChoices.PROCESSING,
+        Order.OrderStatusChoices.CANCELED,
+        Order.OrderStatusChoices.ISSUE,
+    ]
+    print(is_active)
+    transaction, _ = Transaction.objects.get_or_create(
+        product=item.product,
+        order_item=item,
+        quantity=item.quantity,
+    )
+    transaction.transaction_type = transaction_type
+    transaction.is_active = is_active
+    transaction.save()
+
+    related_reserve = Reserve.objects.get(order=order_instance, reserved_item=item.product)
+
+    # Update is_active based on the conditions
+    if order_instance.status in [
+        Order.OrderStatusChoices.CANCELED,
+        Order.OrderStatusChoices.ISSUE,
+    ]:
+        related_reserve.is_active = False
+    elif is_active:
+        related_reserve.is_active = False
+    else:
+        related_reserve.is_active = True
+
+    related_reserve.save()
