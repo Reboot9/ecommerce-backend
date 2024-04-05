067a95329bed2f7cb1bc1e1e6738db8f35d048c2
Reboot <sergejskorikjr@gmail.com>
2024-04-05 16:47:49 +0300

    feat: add update logic to cart serializer

--- apps/cart/serializers/cart_item.py
+++ apps/cart/serializers/cart_item.py
@@ -10,7 +10,7 @@ class CartItemSerializer(serializers.ModelSerializer):
     """Serializer for goods in Cart."""
 
     cartID = serializers.UUIDField(read_only=True, source="cart_id")
-    productID = serializers.UUIDField(required=True, source="product_id")
+    productID = serializers.UUIDField(read_only=True, source="product_id")
     discountPercentage = serializers.DecimalField(
         source="discount_percentage", max_digits=5, decimal_places=2, default=0, read_only=True
     )
@@ -19,3 +19,11 @@ class CartItemSerializer(serializers.ModelSerializer):
         model = CartItem
         fields = ["id", "cartID", "productID", "quantity", "price", "discountPercentage", "cost"]
         read_only_fields = ["id", "cartID", "price", "discountPercentage", "cost"]
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing CartItem instance, but only for changing the quantity field.
+        """
+        instance.quantity = validated_data.get("quantity", instance.quantity)
+        instance.save()
+        return instance
