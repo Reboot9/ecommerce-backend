8a1fa3c8b416f5d2f6df2ff6e0c7e5775b721d27
Reboot <sergejskorikjr@gmail.com>
2024-04-10 16:59:12 +0300

    feat(cart): disable create and update methods inside of Cart serializer

--- apps/cart/serializers/cart.py
+++ apps/cart/serializers/cart.py
@@ -8,7 +8,6 @@ from rest_framework import serializers
 
 from apps.cart.models import Cart
 from apps.cart.serializers.cart_item import CartItemSerializer
-from apps.cart.services.cart import create_or_update_cart, update_cart
 
 User = get_user_model()
 
@@ -19,7 +18,7 @@ class CartSerializer(serializers.ModelSerializer):
     totalQuantity = serializers.IntegerField(source="total_quantity", read_only=True)
     totalPrice = serializers.DecimalField(
         source="total_price",
-        max_digits=10,
+        max_digits=5,
         decimal_places=2,
         read_only=True,
     )
@@ -31,14 +30,14 @@ class CartSerializer(serializers.ModelSerializer):
         fields = ["id", "userID", "items", "totalQuantity", "totalPrice"]
         read_only_fields = ["id", "userID", "totalQuantity", "totalPrice"]
 
-    def create(self, validated_data) -> Cart:
-        """Create Cart with goods. Use the POST method."""
-        items = validated_data.pop("items")
-        user = self.context["user"]
-        cart = create_or_update_cart(items, user)
-        return cart
-
-    def update(self, instance, validated_data) -> Cart:
-        """Update item in Cart. Use the PATCH method."""
-        items = validated_data.pop("items")
-        return update_cart(instance, items)
+    # def create(self, validated_data) -> Cart:
+    #     """Create Cart with goods. Use the POST method."""
+    #     items = validated_data.pop("items")
+    #     user = self.context["user"]
+    #     cart = create_or_update_cart(items, user)
+    #     return cart
+    #
+    # def update(self, instance, validated_data) -> Cart:
+    #     """Update item in Cart. Use the PATCH method."""
+    #     items = validated_data.pop("items")
+    #     return update_cart(instance, items)
