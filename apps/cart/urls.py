9cd31ea86236c7cee21a0df3670617988bc814c0
Reboot <sergejskorikjr@gmail.com>
2024-04-10 17:07:23 +0300

    feat(cart): update urls and views to a more RESTful appearance

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -6,15 +6,12 @@ from rest_framework.routers import SimpleRouter
 
 from apps.cart.views import CartViewSet, CartItemViewSet
 
-cart_router = SimpleRouter()
-cart_router.register(prefix="carts", viewset=CartViewSet, basename="carts")
-
-cart_item_router = SimpleRouter()
-cart_item_router.register(prefix="cart_items", viewset=CartItemViewSet, basename="cart_items")
+router = SimpleRouter()
+router.register(prefix=r"carts/items", viewset=CartItemViewSet, basename="cart_items")
+router.register(prefix=r"carts", viewset=CartViewSet, basename="carts")
 
 app_name = "cart"
 
 urlpatterns = [
-    path("", include(cart_router.urls)),
-    path("", include(cart_item_router.urls)),
+    path("", include(router.urls)),
 ]
