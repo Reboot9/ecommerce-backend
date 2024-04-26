34a5a4516cff589bc0b75c32a21fda946f8503c7
Reboot <sergejskorikjr@gmail.com>
2024-04-26 18:55:33 +0300

    refactor: rename CustomUser verbose name to User

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,6 +4,7 @@ Defines the custom user model and related manager for authentication and authori
 
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
@@ -76,6 +77,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         indexes = [
             models.Index(fields=["email"]),
         ]
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
 
     def __str__(self) -> str:
         """
