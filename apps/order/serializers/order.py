40565adae9eed05a15bfb1355c8277d5d0bbb663
Reboot <sergejskorikjr@gmail.com>
2024-04-30 18:39:24 +0300

    feat: add logic to create order via cart

--- apps/order/serializers/order.py
+++ apps/order/serializers/order.py
@@ -3,9 +3,13 @@ Module: order.py.
 
 This module defines the serializers for order models.
 """
+from decimal import Decimal
+
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
 from apps.base.serializers import BaseDateSerializer
+from apps.cart.models import Cart
 from apps.order.models.order import Order
 from apps.order.serializers.delivery import DeliverySerializer
 from apps.order.serializers.order_item import OrderItemSerializer
@@ -18,13 +22,23 @@ class OrderSerializer(BaseDateSerializer, serializers.ModelSerializer):
 
     items = OrderItemSerializer(
         many=True,
+        required=False,
     )
-    firstName = serializers.CharField(source="first_name")
+    firstName = serializers.CharField(
+        source="first_name",
+        required=True,
+    )
     lastName = serializers.CharField(source="last_name")
-    isPaid = serializers.BooleanField(source="is_paid", default=False, required=False)
+    email = serializers.EmailField(required=True)
+    isPaid = serializers.BooleanField(source="is_paid", read_only=True, default=False)
     orderNumber = serializers.IntegerField(source="order_number", read_only=True)
-    delivery = DeliverySerializer()
+    delivery = DeliverySerializer(required=False)
     comment = serializers.CharField(default="")
+    cost = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    # Additional field for cart_id
+    cartID = serializers.UUIDField(write_only=True, required=False)
 
     class Meta:
         model = Order
@@ -37,25 +51,62 @@ class OrderSerializer(BaseDateSerializer, serializers.ModelSerializer):
             "phone",
             "email",
             "isPaid",
+            "cost",
             "comment",
             "items",
             "delivery",
+            "cartID",
         ]
-        read_only_fields = ["id", "orderNumber", "status", "isPaid"]
+        read_only_fields = ["id", "orderNumber", "status", "isPaid", "cost"]
+
+    def validate(self, attrs):
+        """Overriding validation to provide custom creation logic depending on params."""
+        cart_id = attrs.get("cart_id")
+        if cart_id and "items" in attrs:
+            del attrs["items"]  # Remove "items" if cart_id is provided
+        return attrs
 
     def create(self, validated_data) -> Order:
-        """Create order."""
-        order_items = validated_data.pop("items")
-        delivery = validated_data.pop("delivery")
+        """
+        Create an Order instance based on provided validated data.
+
+        Note, that there's two possible ways of creating an order: via items from cart when there's
+        cartID in json data or by providing order items itself.
+        """
+        cart_id = validated_data.pop("cartID", None)
+        if cart_id:
+            cart = get_object_or_404(
+                Cart, is_active=True, user=self.context["request"].user, id=cart_id
+            )
+            validated_data["items"] = [
+                {"productID": item.product.id, "quantity": item.quantity}
+                for item in cart.items.all()
+            ]
+
+        order_items = validated_data.pop("items", None)
+        delivery = validated_data.pop("delivery", None)
         if order_items is None:
-            raise serializers.ValidationError("'items' is required when creating an instance.")
+            raise serializers.ValidationError(
+                "'items' or `cartID` is required when creating an instance."
+            )
         if delivery is None:
             raise serializers.ValidationError("`delivery` is required when creating an instance.")
 
         order_instance = create_order(order_items, delivery, validated_data)
 
+        # order_number = str(order_instance.id)
+        # cost = str(order_instance.total_order_price)
+        #
         # payment = Payment()
-        # payment_data = validated_data
-        # payment_response = payment.liqpay.api('request', payment_data)
+        # payment_response = payment.generate_new_url_for_pay(order_number, cost)
 
         return order_instance
+
+    def get_cost(self, instance) -> Decimal:
+        """
+        Method to obtain total cost of order.
+
+        :param instance: Order instance.
+        :return: cost of the order
+        """
+        return instance.total_order_price
