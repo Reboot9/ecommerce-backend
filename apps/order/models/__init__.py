08b98b6539fbef03310e8bc7653aba3cb7b11f2e
Reboot <sergejskorikjr@gmail.com>
2024-05-15 13:22:20 +0300

    refactor: modify __init__ for order to include all models in import

---
+++ apps/order/models/__init__.py
@@ -0,0 +1,9 @@
+from apps.order.models.order import Order
+from apps.order.models.order_item import OrderItem
+from apps.order.models.delivery import Delivery
+
+__all__ = [
+    "Order",
+    "OrderItem",
+    "Delivery",
+]
