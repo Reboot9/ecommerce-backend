c566b10552fcf2f74bce95f0b8bae28c8f207f4c
Reboot <sergejskorikjr@gmail.com>
2024-05-21 16:24:06 +0300

    chore: add caching for cart and cart items

--- apps/cart/views/cart.py
+++ apps/cart/views/cart.py
@@ -1,10 +1,12 @@
 """
 This module contains necessary Cart views for the cart app.
 """
+from django.core.cache import cache
 from rest_framework import permissions
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
+from apps.base.mixins import CACHE_TTL
 from apps.cart.models import Cart
 from apps.cart.serializers import CartSerializer
 
@@ -22,6 +24,14 @@ class CartViewSet(viewsets.ModelViewSet):
 
         return [permissions.IsAuthenticated()]
 
+    def get_cache_key(self, cart_id) -> str:
+        """
+        Method to get cache key for the cart list.
+
+        :return: cache key for the cart.
+        """
+        return f"carts:{cart_id}:{self.request.path}?{self.request.GET.urlencode()}"
+
     def get_queryset(self):
         """
         Retrieves the queryset of carts based on the user.
@@ -32,6 +42,21 @@ class CartViewSet(viewsets.ModelViewSet):
         # Logic to filter carts based on the user
         queryset = Cart.objects.filter(user=user, is_active=True)
         return queryset
+
+    def list(self, request, *args, **kwargs) -> Response:
+        """
+        Override the list method to add caching.
+        """
+        cart = Cart.objects.filter(user=self.request.user, is_active=True).first()
+        cache_key = self.get_cache_key(cart.id)
+        cached_data = cache.get(cache_key)
+
+        if cached_data:
+            return Response(cached_data)
+
+        response = super().list(request, *args, **kwargs)
+        cache.set(cache_key, response.data, timeout=CACHE_TTL)
+        return response
 
     def get_object(self):
         """
@@ -88,3 +113,32 @@ class CartViewSet(viewsets.ModelViewSet):
         cart_instance.is_active = False
         cart_instance.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        """
+        Override perform_destroy to clear cache after deleting a cart.
+        """
+        # Clear the list cache when a cart is deleted
+        cache.delete(self.get_cache_key(instance.id))
+
+    def perform_create(self, serializer):
+        """
+        Perform actions when creating instance.
+
+        :param serializer: The serializer instance used for validation and saving.
+        :return:
+        """
+        instance = serializer.save()
+        cache.delete(self.get_cache_key(instance.id))
+        return instance
+
+    def perform_update(self, serializer):
+        """
+        Override perform_update to clear cache after updating a cart item.
+
+        :param serializer: The serializer instance used for validation and saving.
+        """
+        instance = serializer.save()
+        # Clear the list cache when a cart item is updated
+        cache.delete(self.get_cache_key(instance.id))
+        return instance
